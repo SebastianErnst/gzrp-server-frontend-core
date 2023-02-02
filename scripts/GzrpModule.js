e809739cd7b39a711069dece5bd545724e0d0666
Basti <ernst.sebastian1991@mail.com>
2023-02-02 18:18:23 +0100

    Fügt Basisklasse für alle GzrpModule ein

---
+++ scripts/GzrpModule.js
@@ -0,0 +1,16 @@
+import NUICallbackHandler from 'NUICallbackHandler';
+
+class GzrpModule {
+    constructor() {
+        this.ingame = true;
+        this.$body = document.querySelector('body');
+        this.NUICallbackHandler = new NUICallbackHandler();
+    }
+    activateModule() {
+        this.$body.classList.add('is-active');
+    }
+
+    deactivateModule() {
+        this.$body.classList.remove('is-active');
+    }
+}

5aab67fe387f05002641d617d006c1e6e2080f84
Basti <ernst.sebastian1991@mail.com>
2023-02-04 21:30:19 +0100

    Baut Weiche für ingame und out of game ein

--- scripts/GzrpModule.js
+++ scripts/GzrpModule.js
@@ -3,13 +3,15 @@ import NUICallbackHandler from 'NUICallbackHandler';
 export default class GzrpModule {
     constructor() {
         this.ingame = true;
+        if (location.hostname === "localhost") {
+            this.ingame = false;
+        }
         this.$body = document.querySelector('body');
         this.NUICallbackHandler = new NUICallbackHandler();
     }
     activateModule() {
         this.$body.classList.add('is-active');
     }
-
     deactivateModule() {
         this.$body.classList.remove('is-active');
     }

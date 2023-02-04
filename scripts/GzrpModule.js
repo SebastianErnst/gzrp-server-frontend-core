e7db2344710c1f1fcf14bc1d14c1dd97e9191c55
Basti <ernst.sebastian1991@mail.com>
2023-02-05 00:01:04 +0100

    Ach wie is das schön

--- scripts/GzrpModule.js
+++ scripts/GzrpModule.js
@@ -3,11 +3,13 @@ import NUICallbackHandler from 'NUICallbackHandler';
 export default class GzrpModule {
     constructor() {
         this.ingame = true;
-        if (location.hostname === "localhost") {
-            this.ingame = false;
-        }
         this.$body = document.querySelector('body');
         this.NUICallbackHandler = new NUICallbackHandler();
+
+        if (location.hostname === "localhost") {
+            this.ingame = false;
+            this.$body.classList.add('development');
+        }
     }
     activateModule() {
         this.$body.classList.add('is-active');
